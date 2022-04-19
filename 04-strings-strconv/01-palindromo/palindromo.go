bdcf73bcea89663c49dccd5dff3c3a6bf26511bf
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-19 17:42:37 +0200

    Sección 9 - Replace y ReplaceAll

--- 04-strings-strconv/01-palindromo/palindromo.go
+++ 04-strings-strconv/01-palindromo/palindromo.go
@@ -7,11 +7,15 @@ import (
 
 func esPalindromo(palabra string) {
 
-	fmt.Println(palabra)
-	palabra = strings.ToLower(palabra)
+	/*fmt.Println(palabra)
+	palabra = strings.ToLower(palabra)*/
 
 	fmt.Println(palabra)
 	palabra = strings.ToUpper(palabra)
+	fmt.Println(palabra)
+
+	//palabra = strings.Replace(palabra, "Z", "S", 2)ç
+	palabra = strings.ReplaceAll(palabra, " ", "")
 	fmt.Println(palabra)
 
 }
@@ -20,6 +24,7 @@ func main() {
 
 	// Una palabra palíndroma es que se lee de igual forma de izquierda a derecha y viceversa
 	// Un ejemplo podría ser la palabra "oso"
-	esPalindromo("Oso")
+	//esPalindromo("Oso")
+	esPalindromo("Luz azul") // También es una palabra palíndroma si le quitamos ese espacio
 
 }
