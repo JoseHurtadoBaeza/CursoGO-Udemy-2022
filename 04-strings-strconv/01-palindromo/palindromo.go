cfe113d7929b4c0162409ad40ef4b8e5507fb6c5
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-20 20:29:30 +0200

    Sección 9 - Join terminado

--- 04-strings-strconv/01-palindromo/palindromo.go
+++ 04-strings-strconv/01-palindromo/palindromo.go
@@ -8,24 +8,35 @@ import (
 // Función que devuelve la cadena pasada por parámetro al revés
 func reverse(cadena string) string {
 
-	arrayCadena := strings.Split(cadena, " ")
-	fmt.Println(arrayCadena)
+	arrayCadena := strings.Split(cadena, "")
+	arrayInvertida := make([]string, 0)
 
-	return ""
+	for i := len(arrayCadena) - 1; i >= 0; i-- {
+		arrayInvertida = append(arrayInvertida, arrayCadena[i])
+	}
+	//fmt.Println(arrayCadena)
+	//fmt.Println(arrayInvertida)
+
+	return strings.Join(arrayInvertida, "")
 }
 
-func esPalindromo(palabra string) {
+func esPalindromo(palabra string) bool {
 
-	/*fmt.Println(palabra)
-	palabra = strings.ToLower(palabra)*/
+	//fmt.Println(palabra)
+	palabra = strings.ToLower(palabra)
 
-	fmt.Println(palabra)
-	palabra = strings.ToUpper(palabra)
-	fmt.Println(palabra)
+	//fmt.Println(palabra)
+	//palabra = strings.ToUpper(palabra)
+	//fmt.Println(palabra)
 
 	//palabra = strings.Replace(palabra, "Z", "S", 2)ç
 	palabra = strings.ReplaceAll(palabra, " ", "")
-	fmt.Println(palabra)
+	//fmt.Println(palabra)
+
+	//fmt.Println(reverse(palabra))
+	palabraInvertida := reverse(palabra)
+
+	return palabra == palabraInvertida
 
 }
 
@@ -34,8 +45,12 @@ func main() {
 	// Una palabra palíndroma es que se lee de igual forma de izquierda a derecha y viceversa
 	// Un ejemplo podría ser la palabra "oso"
 	//esPalindromo("Oso")
-	esPalindromo("Luz azul") // También es una palabra palíndroma si le quitamos ese espacio
+	if esPalindromo("ANA") { // También es una palabra palíndroma si le quitamos ese espacio
+		fmt.Println("Es palíndromo")
+	} else {
+		fmt.Println("No es palíndromo")
+	}
 
-	reverse("Luz azul")
+	//reverse("Luz azul")
 
 }
