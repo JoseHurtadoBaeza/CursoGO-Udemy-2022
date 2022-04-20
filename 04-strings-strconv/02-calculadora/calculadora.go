5d980b5a42958205189b84dcf5e355eb83434da3
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-20 21:54:02 +0200

    Sección 9 - Calculadora y strconv terminados

--- 04-strings-strconv/02-calculadora/calculadora.go
+++ 04-strings-strconv/02-calculadora/calculadora.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func sumar(expresion string) {
+func sumar(expresion string) int {
 
 	// 4+4+5+8
 	valores := strings.Split(expresion, "+")
 
-	fmt.Printf("%T \n", valores)
+	var result int
+
+	for _, valor := range valores {
+		num, _ := strconv.Atoi(valor)
+
+		result += num
+	}
+
+	return result
 
 }
 
 func main() {
 
 	var expresion string
-	//var result int
+	var result int
 
 	fmt.Printf("=>")
 	fmt.Scanln(&expresion)
 
-	sumar(expresion)
+	result = sumar(expresion)
+
+	fmt.Printf("=> %d\n", result)
 
 }
