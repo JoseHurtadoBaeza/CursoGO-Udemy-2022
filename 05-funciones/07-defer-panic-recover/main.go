2434765031f73d2329f70d15ceacc95c4cfa321f
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-29 03:11:56 +0200

    Sección 10 - Defer

--- 05-funciones/07-defer-panic-recover/main.go
+++ 05-funciones/07-defer-panic-recover/main.go
@@ -7,17 +7,17 @@ import (
 
 func main() {
 
-	file, error := os.Open("hola.txt")
-
-	if error != nil {
+	if file, error := os.Open("hola.txt"); error != nil {
 		fmt.Println("No fue posible de esta forma")
 	} else {
+		defer func() { // Se ejecutará al final
+			fmt.Println("Cerrando el archivo!")
+			file.Close()
+		}()
 		contenido := make([]byte, 254)
-		long, _ := file.Read(contenido)
+		long, _ := file.Read(contenido) // Devuelve la longitud y el error
 		contenidoArchivo := string(contenido[:long])
 		fmt.Println(contenidoArchivo)
 	}
-
-	file.Close()
 
 }
