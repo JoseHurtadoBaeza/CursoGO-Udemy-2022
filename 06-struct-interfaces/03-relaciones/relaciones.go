14393cacf42832c0946697bd23bd61e5bc716937
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-30 18:17:03 +0200

    Sección 11 - Relación de uno a muchos

--- 06-struct-interfaces/03-relaciones/relaciones.go
+++ 06-struct-interfaces/03-relaciones/relaciones.go
@@ -13,26 +13,57 @@ type Student struct {
 	codigo string
 }
 
+// Relación de uno a muchos
+type Curso struct {
+	titulo string
+	videos []Video
+}
+
+type Video struct {
+	titulo string
+	curso  Curso
+}
+
 func main() {
 
-	jose := User{
-		nombre: "Jose",
-		email:  "jose@gmail.com",
-		activo: true,
+	// Relación de uno a uno
+	/*
+		jose := User{
+			nombre: "Jose",
+			email:  "jose@gmail.com",
+			activo: true,
+		}
+
+		carlos := User{
+			nombre: "Carlos",
+			email:  "carlos@gmail.com",
+			activo: true,
+		}
+
+		joseStudent := Student{
+			user:   jose,
+			codigo: "001arsd",
+		}
+
+		fmt.Println(carlos)
+		fmt.Println(joseStudent.user.nombre)*/
+
+	// Relación de uno a muchos
+	video1 := Video{titulo: "01-Introducción"}
+	video2 := Video{titulo: "02-Instalación"}
+
+	curso := Curso{
+		titulo: "Curso de GO",
+		videos: []Video{video1, video2},
 	}
 
-	carlos := User{
-		nombre: "Carlos",
-		email:  "carlos@gmail.com",
-		activo: true,
-	}
+	video1.curso = curso
+	video2.curso = curso
 
-	joseStudent := Student{
-		user:   jose,
-		codigo: "001arsd",
-	}
+	fmt.Println(video1.curso.titulo)
 
-	fmt.Println(carlos)
-	fmt.Println(joseStudent.user.nombre)
+	for _, video := range curso.videos {
+		fmt.Println(video.titulo)
+	}
 
 }
