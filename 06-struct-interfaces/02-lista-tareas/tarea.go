df31ac3165449d2cc493e39fc460aaaf70c5eecd
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-30 03:34:33 +0200

    Sección 11 - PRÁCTICA: Lista de Tareas

--- 06-struct-interfaces/02-lista-tareas/tarea.go
+++ 06-struct-interfaces/02-lista-tareas/tarea.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// Lista de tareas
+type TaskList struct {
+	tasks []*Task
+}
+
+func (tl *TaskList) appendTask(t *Task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *TaskList) removeTask(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
 // Tareas
 type Task struct {
 	name      string
@@ -31,7 +44,28 @@ func main() {
 		completed: true,
 	}
 
-	t1.toPrint()
-	t2.toPrint()
+	lista := TaskList{}
+	lista.appendTask(&t1)
+	lista.appendTask(&t2)
+
+	fmt.Println(lista)
+
+	lista.removeTask(1)
+
+	t3 := Task{
+		name:      "Curso de CSS",
+		desc:      "Completar curso de CSS esta semana",
+		completed: false,
+	}
+
+	lista.appendTask(&t3)
+	fmt.Println(lista)
+
+	for i, task := range lista.tasks {
+		fmt.Println(i, task.name)
+	}
+
+	//t1.toPrint()
+	//t2.toPrint()
 
 }
