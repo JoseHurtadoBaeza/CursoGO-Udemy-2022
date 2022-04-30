ec00901560b90d89e8a4c4e7ec8c5faeb8cf7401
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-04-30 03:03:25 +0200

    Sección 11 - Métodos

--- 06-struct-interfaces/01-struct/main.go
+++ 06-struct-interfaces/01-struct/main.go
@@ -8,21 +8,40 @@ type Persona struct {
 	edad   int
 }
 
+// Métodos
+func (p *Persona) imprimir() {
+
+	fmt.Printf("Nombre: %s\nEdad: %d\n", p.nombre, p.edad)
+
+}
+
+func (p *Persona) editNombre(nuevoNombre string) {
+
+	p.nombre = nuevoNombre
+
+}
+
 func main() {
 
 	p1 := Persona{"Jose", 25}
 
-	fmt.Println(p1)
+	//fmt.Println(p1)
+	p1.imprimir()
 
-	p1.nombre = "Carlos"
+	//p1.nombre = "Carlos"
+	p1.editNombre("Carlos")
 
-	fmt.Println(p1)
+	//fmt.Println(p1)
+	p1.imprimir()
 
 	p2 := Persona{
 		nombre: "Pepe",
 		edad:   62,
 	}
 
-	fmt.Println(p2)
+	//fmt.Println(p2)
+	p2.imprimir()
+	p2.editNombre("Juan")
+	p2.imprimir()
 
 }
