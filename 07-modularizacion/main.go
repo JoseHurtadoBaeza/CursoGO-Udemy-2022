388ebb238c7b4d322ba1ea96f3e1802154d39c46
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-05-01 03:02:05 +0200

    Sección 12 - Instalar módulos de terceros

--- 07-modularizacion/main.go
+++ 07-modularizacion/main.go
@@ -1,14 +1,14 @@
 // Un detalle a tener en cuenta es que si evitamos usar guiones en las rutas la importación de paquetes se hará de forma más automatizada
 // y sino lo que tenemos que hacer es lo que hemos hecho aquí, que básicamente consiste en ejecutar el comando go mod init "nombre del módulo gestor de paquetes"
-// y tras esto sólamente tendremos que usar los import cómo se puede ver en este ejemplo.
+// y tras esto sólamente tendremos que hacer el import.
 package main
 
-import (
-	"fmt"
-	"paquetes/models"
-)
+import "github.com/donvito/hellomod"
 
 func main() {
+
+	hellomod.SayHello()
+
 	/*mensajes.Hola()
 	mensajes.Imprimir()*/
 	/*cua1 := figuras.Cuadrado{Lado: 8}
@@ -17,11 +17,11 @@ func main() {
 	figuras.Medidas(&cua1)
 	figuras.Medidas(&cir1)*/
 
-	p1 := models.Persona{}
+	/*p1 := models.Persona{}
 	p1.Constructor("Jose", 25)
 	fmt.Println(p1)
 	fmt.Println(p1.GetNombre())
 	p1.SetNombre("Carlos")
 
-	fmt.Println(p1)
+	fmt.Println(p1)*/
 }
