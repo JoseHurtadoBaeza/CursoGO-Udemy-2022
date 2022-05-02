8a079dd57a3b38f062ce81c81515c91588a6ab41
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-05-03 00:54:08 +0200

    Seccion 13 - Sin concurrencia

---
+++ 08-concurrencia/servidores/servidores.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Esta función se va a encargar de comprobar si un servidor está funcionando o no
+// Recibe la url del servidor por parámetro
+func revisarServidor(servidor string) {
+	_, err := http.Get(servidor)
+
+	if err != nil {
+		fmt.Println("No está disponible")
+	} else {
+		fmt.Println(servidor, "Está funcionando")
+	}
+
+}
+
+func main() {
+
+	inicio := time.Now()
+
+	servidores := []string{
+		"https://oregoom.com/",
+		"https://www.udemy.com/",
+		"https://www.youtube.com/",
+		"https://www.facebook.com/",
+		"https://www.google.com/",
+	}
+
+	for _, servidor := range servidores {
+		revisarServidor(servidor)
+	}
+
+	tiempoPaso := time.Since(inicio)
+
+	fmt.Println("Tiempo de ejecución: ", tiempoPaso)
+
+}
