24824e7937e704297cf4d3146bb5033afced814a
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-01 17:18:31 +0200

    Sección 15 - Envío de errores

--- goweb/01-introduccion/goweb.go
+++ goweb/01-introduccion/goweb.go
@@ -16,11 +16,18 @@ func PaginaNF(rw http.ResponseWriter, r *http.Request) {
 	http.NotFound(rw, r)
 }
 
+func Error(rw http.ResponseWriter, r *http.Request) {
+	//http.Error(rw, "La página no funciona", 404)
+	// En vez de utilizar los valores enteros, GO nos recomienda usar las constantes que podemos consultar en https://pkg.go.dev/net/http#pkg-constants
+	http.Error(rw, "La página no funciona", http.StatusNotFound)
+}
+
 func main() {
 
 	// Router
 	http.HandleFunc("/", Hola)
 	http.HandleFunc("/page", PaginaNF)
+	http.HandleFunc("/error", Error)
 
 	// Crear servidor
 	//http.ListenAndServe("localhost:3000", nil)
