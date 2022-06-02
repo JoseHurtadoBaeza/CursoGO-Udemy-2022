30bba73b7363a4c7076f884073ed44cfd93935a3
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-02 20:57:58 +0200

    Sección 18 - Crear tabla

--- goweb/04-go-mysql/db/database.go
+++ goweb/04-go-mysql/db/database.go
@@ -37,3 +37,8 @@ func Ping() {
 		panic(err)
 	}
 }
+
+// Crea una tabla
+func CreateTable(schema string) {
+	db.Exec(schema)
+}
--- goweb/04-go-mysql/main.go
+++ goweb/04-go-mysql/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "gomysql/db"
+import (
+	"gomysql/db"
+	"gomysql/models"
+)
 
 func main() {
 	db.Connect()
 	db.Ping() // Verificamos la conexión
+
+	db.CreateTable(models.UserSchema)
+
 	db.Close()
 	// Si la ejecutamos después de cerrar la BD nos dará un panic
 	//db.Ping()
