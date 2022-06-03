0ca7c48c575bd9b66f25ed3e700281300de9a733
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-03 03:01:47 +0200

    Sección 19 - Listar registros

--- goweb/05-api-rest/handlers/handlers.go
+++ goweb/05-api-rest/handlers/handlers.go
@@ -1,12 +1,20 @@
 package handlers
 
 import (
+	"apirest/db"
+	"apirest/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Lista todos los usuarios")
+	//fmt.Fprintln(rw, "Lista todos los usuarios")
+	db.Connect()
+	users := models.ListUsers()
+	db.Close()
+	output, _ := json.Marshal(users) // Marshall devuelve 2 valores: Los valores transformados en tipo byte y un error
+	fmt.Fprintln(rw, string(output))
 }
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
