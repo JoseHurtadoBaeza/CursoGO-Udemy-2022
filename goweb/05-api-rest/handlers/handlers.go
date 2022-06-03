c87bc9d8fc424f0181735dde6bf0c54d81e55489
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-03 04:04:12 +0200

    Sección 19 - Realizar un registro

--- goweb/05-api-rest/handlers/handlers.go
+++ goweb/05-api-rest/handlers/handlers.go
@@ -51,7 +51,30 @@ func GetUser(rw http.ResponseWriter, r *http.Request) {
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Crea un usuario")
+	//fmt.Fprintln(rw, "Crea un usuario")
+
+	rw.Header().Set("content-type", "application/json") // Para responder con json
+	//rw.Header().Set("content-type", "text/xml") // Para responder con xml
+	// No hay un tipo de dato específico para yaml
+
+	// Obtener registro
+	user := models.User{}
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&user); err != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		user.Save() // Método creado por nosotros que inserta o actualiza dependiendo de si el usuario existe ya o no
+		db.Close()
+	}
+
+	// Marshall devuelve 2 valores: Los valores transformados en tipo byte y un error
+	output, _ := json.Marshal(user) // Para responder con json
+	//output, _ := xml.Marshal(users) // Para responder con xml
+	//output, _ := yaml.Marshal(users) // Para responder con yaml
+	fmt.Fprintln(rw, string(output))
 }
 
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
