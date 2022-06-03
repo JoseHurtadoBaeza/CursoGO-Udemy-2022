8e260beec253c593d32715c6219d49d977084d2a
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-03 03:55:14 +0200

    Sección 19 - Obtener un registro

--- goweb/05-api-rest/handlers/handlers.go
+++ goweb/05-api-rest/handlers/handlers.go
@@ -6,6 +6,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
@@ -26,7 +29,25 @@ func GetUsers(rw http.ResponseWriter, r *http.Request) {
 }
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Obtiene un usuario")
+	//fmt.Fprintln(rw, "Obtiene un usuario")
+
+	rw.Header().Set("content-type", "application/json") // Para responder con json
+	//rw.Header().Set("content-type", "text/xml") // Para responder con xml
+	// No hay un tipo de dato específico para yaml
+
+	// Obtener ID
+	vars := mux.Vars(r) // Mos devuelve un mapa de tipo string
+	userId, _ := strconv.Atoi(vars["id"])
+
+	db.Connect()
+	user := models.GetUser(userId)
+	db.Close()
+	// Marshall devuelve 2 valores: Los valores transformados en tipo byte y un error
+	output, _ := json.Marshal(user) // Para responder con json
+	//output, _ := xml.Marshal(users) // Para responder con xml
+	//output, _ := yaml.Marshal(users) // Para responder con yaml
+	fmt.Fprintln(rw, string(output))
+
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
