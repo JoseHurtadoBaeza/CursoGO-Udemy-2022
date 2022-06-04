edb00c6cc852ec79944104a80895b96e136212a8
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2022-06-04 03:14:14 +0200

    Sección 20 - Delete

--- goweb/06-api-rest-orm/handlers/handlers.go
+++ goweb/06-api-rest-orm/handlers/handlers.go
@@ -210,36 +210,43 @@ func UpdateUser(rw http.ResponseWriter, r *http.Request) {
 
 }
 
-///func DeleteUser(rw http.ResponseWriter, r *http.Request) {
-// EL CÓDIGO COMENTADO ES ANTERIOR A LA REFACTORIZACIÓN FINAL
-//fmt.Fprintln(rw, "Elimina un usuario")
+func DeleteUser(rw http.ResponseWriter, r *http.Request) {
+	// EL CÓDIGO COMENTADO ES ANTERIOR A LA REFACTORIZACIÓN FINAL
+	//fmt.Fprintln(rw, "Elimina un usuario")
 
-/*rw.Header().Set("content-type", "application/json") // Para responder con json
-//rw.Header().Set("content-type", "text/xml") // Para responder con xml
-// No hay un tipo de dato específico para yaml
+	/*rw.Header().Set("content-type", "application/json") // Para responder con json
+	  //rw.Header().Set("content-type", "text/xml") // Para responder con xml
+	  // No hay un tipo de dato específico para yaml
 
-// Obtener ID
-vars := mux.Vars(r) // Mos devuelve un mapa de tipo string
-userId, _ := strconv.Atoi(vars["id"])
+	  // Obtener ID
+	  vars := mux.Vars(r) // Mos devuelve un mapa de tipo string
+	  userId, _ := strconv.Atoi(vars["id"])
 
-db.Connect()
-user, _ := models.GetUser(userId)
-user.Delete()
-db.Close()
-// Marshall devuelve 2 valores: Los valores transformados en tipo byte y un error
-output, _ := json.Marshal(user) // Para responder con json
-//output, _ := xml.Marshal(users) // Para responder con xml
-//output, _ := yaml.Marshal(users) // Para responder con yaml
-fmt.Fprintln(rw, string(output))*/
+	  db.Connect()
+	  user, _ := models.GetUser(userId)
+	  user.Delete()
+	  db.Close()
+	  // Marshall devuelve 2 valores: Los valores transformados en tipo byte y un error
+	  output, _ := json.Marshal(user) // Para responder con json
+	  //output, _ := xml.Marshal(users) // Para responder con xml
+	  //output, _ := yaml.Marshal(users) // Para responder con yaml
+	  fmt.Fprintln(rw, string(output))*/
 
-/*	if user, err := getUserByRequest(r); err != nil {
-		models.SendNotFound(rw)
+	/*	if user, err := getUserByRequest(r); err != nil {
+			models.SendNotFound(rw)
+		} else {
+			user.Delete()
+			models.SendData(rw, user)
+		}*/
+
+	if user, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
 	} else {
-		user.Delete()
-		models.SendData(rw, user)
+		db.Database.Delete(&user)
+		sendData(rw, user, http.StatusOK)
 	}
 
-}*/
+}
 
 // DEPRECATED
 // Función reutilizable para simplificar el código en el uso
